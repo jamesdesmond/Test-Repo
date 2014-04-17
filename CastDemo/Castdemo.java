e3a09840fcb58cd49745eeb5f0369b7251abed0d
James Desmond <jamesdesmond13@gmail.com>
2014-04-17 19:21:58 -0400

    Notfunctional CastDemo

---
+++ CastDemo/Castdemo.java
@@ -0,0 +1,13 @@
+class CastDemo
+{
+    public static void main(String[] Args)
+    {
+        double x, y;
+        byte b;
+        int i;
+        char ch;
+
+        x = 10.0;
+        y = 3.0;
+
+        i = 
