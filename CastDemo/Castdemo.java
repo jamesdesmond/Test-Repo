bb37ff8cbc3fe9732fd7576087658673ef34bcf9
James Desmond <jamesdesmond13@gmail.com>
2014-04-17 19:36:39 -0400

    Finished product, fully commented

--- CastDemo/Castdemo.java
+++ CastDemo/Castdemo.java
@@ -13,4 +13,16 @@ class CastDemo
         i = (int) (x/y); //truncation will occur here
         System.out.println("Integer outcome of x / y: " + i);
 
+        i = 100;
+        b = (byte) i; //byte can hold 100
+        System.out.println("Value of b: " + b);
 
+        i = 257;
+        b = (byte) i; //byte cannot hold 257
+        System.out.println("Value of b: " + B);
+
+        b = 88 //ASCII code for X
+        ch = (char) b;  //the byte gets converted to char value
+        System.out.println("ch: " + ch);
+    }
+}
